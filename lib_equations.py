04fcf48454769b051d53b3015e077aeabb31146a
mvolpato-unimib <m.volpato6@campus.unimib.it>
2026-04-16 12:38:53 +0200

    ex14: implementazione del secant method, mancano diversi vecchi esercizi

--- lib_equations.py
+++ lib_equations.py
@@ -10,15 +10,14 @@ def Part_dervs(func, params, eps=1e-8):
     return part_der
 
 
-def bisec(f, a, b, optim=True, tol=1e-14, out_niter=False):
+def bisec(f, a, b, optim=True, tol=1e-14, out_niter=False, out_story=False):
     fa, fb = f(a), f(b)
     if fa * fb > 0:
         raise ValueError('f(a) and f(b) must have different signs')
 
+    story = []
     n_iter = 0
     while True:
-        n_iter += 1
-        
         if optim:
             den = fb - fa
             if abs(den) < tol: 
@@ -37,7 +36,62 @@ def bisec(f, a, b, optim=True, tol=1e-14, out_niter=False):
             a, fa = c, fc
         else:
             b, fb = c, fc
+        
+        story.append(c)
+        n_iter += 1
 
-    return (c, n_iter) if out_niter else c
+    if out_niter: 
+        return (c, n_iter) 
+    elif out_story:
+        return (c, n_iter, story) 
+    else:
+        return c
+
+
+def Newt_Rap(func, der_func, x0, tol=1e-14, MaxIter=1e2):
+    xn = x0
+    f_val = func(xn)
+    f_der_val = der_func(xn)
+    x1 = x0 - f_val / f_der_val    
+    n_iter = 0
+
+    sol_ls = []
+
+    while abs(func(x1)) > tol and n_iter<MaxIter:
+        f_val = func(x1)
+        f_der_val = der_func(x1)
+        x_next = lambda x_old: x_old - f_val / f_der_val    
+        
+        if f_der_val == 0:
+            raise ValueError("Derivative is 0. Newton would break.")
+        x0 = x1
+        x1 = x_next(x0)
+        sol_ls.append(x1)
+        n_iter += 1
+    return x1, n_iter, np.array(sol_ls)
+
+
+
+def Secant_mth(func, x0, x1, tol=1e-14, MaxIter=1e2):
+    der_func = lambda x, x_prev: (func(x) - func(x_prev)) / (x - x_prev)
+    n_iter = 0
+    x_prev = x0
+    x = x1
+    f_val = func(x)
+    f_der_val = der_func(x, x_prev)
+    x_next = x - f_val / f_der_val
+    sol_ls = []
+
+    while abs(f_val) > tol and n_iter < MaxIter:
+        fx_next = lambda x_old: x_old - f_val / f_der_val    
+        if f_der_val == 0:
+            raise ValueError("Derivative is 0. Secant mth would break.")
+        x_prev = x
+        x = fx_next(x)
+        sol_ls.append(x)
+        n_iter += 1
+        f_val = func(x)
+        f_der_val = der_func(x, x_prev)
+    return x, n_iter, np.array(sol_ls)
 
 
